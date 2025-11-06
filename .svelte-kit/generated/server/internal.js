99952c23405b1d45aba9b12a2dcf5d57256d7ee6
Matheus <matheusalbinommarques@gmail.com>
2025-11-06 18:19:09 -0300

    mudanças nas artes

--- .svelte-kit/generated/server/internal.js
+++ .svelte-kit/generated/server/internal.js
@@ -48,7 +48,7 @@ export const options = {
       message +
       '</h1>\n\t\t\t</div>\n\t\t</div>\n\t</body>\n</html>\n',
   },
-  version_hash: '1y7heie',
+  version_hash: 'urt7km',
 };
 
 export async function get_hooks() {
