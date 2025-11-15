d11ca282e6d6c24a1172968c19d9de10d214dda2
Matheus <matheusalbinommarques@gmail.com>
2025-11-15 16:06:18 -0300

    TUDO CERTO GRAÇAS A DEUS

--- .svelte-kit/generated/server/internal.js
+++ .svelte-kit/generated/server/internal.js
@@ -48,7 +48,7 @@ export const options = {
       message +
       '</h1>\n\t\t\t</div>\n\t\t</div>\n\t</body>\n</html>\n',
   },
-  version_hash: '5n5nh',
+  version_hash: '13nz82c',
 };
 
 export async function get_hooks() {
